60f6dabebae45a024803290d72c9abb456b8ee16
Monisha Suthapalli <31736760+MonishaSuthapalli@users.noreply.github.com>
2022-01-06 17:11:27 -0500

    Time: 1 ms (90.00%), Space: 36.9 MB (88.02%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        
+        Arrays.sort(costs,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1,int[] o2)
+            {
+                //Profit gained by sending to A but not B 
+                return o1[0]-o1[1]-(o2[0]-o2[1]);
+            }
+        });
+        
+        int total=0;
+        int n=costs.length/2;
+        for(int i=0;i<n;i++)
+        {
+            total=total+costs[i][0];
+            total+=costs[i+n][1];
+        }
+        
+        return total;
+        
+    }
+}
